415ae3a83fa476f6553104d4258be12179f1a924
popany <popany@163.com>
2023-02-18 16:48:03 +0000

    add

--- demos/google_benchmark_demo/demo/demo.cc
+++ demos/google_benchmark_demo/demo/demo.cc
@@ -6,13 +6,17 @@ const std::vector<double> X = {0.1, 0.4, 1.2, 1.8, 2.0};  // must be increasing
 const std::vector<double> Y = {0.1, 0.7, 0.6, 1.1, 0.9};
 
 static void BM_C2SplineCreation(benchmark::State& state) {
-  tk::spline s(X, Y ,tk::spline::cspline);
+  for (auto _ : state) {
+    tk::spline s(X, Y ,tk::spline::cspline);
+  }
 }
 // Register the function as a benchmark
 BENCHMARK(BM_C2SplineCreation);
 
 static void BM_C1SplineCreation(benchmark::State& state) {
-  tk::spline s(X, Y ,tk::spline::cspline_hermite);
+  for (auto _ : state) {
+    tk::spline s(X, Y ,tk::spline::cspline_hermite);
+  }
 }
 // Register the function as a benchmark
 BENCHMARK(BM_C1SplineCreation);
