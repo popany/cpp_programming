4c8aa9972702fcda3034ffeb1fe2d7200c344536
popany <popany@163.com>
2023-03-19 15:13:12 +0000

    add

--- curve/jupyter/start.sh
+++ curve/jupyter/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 readonly SCRIPT_PATH=$(realpath "$0")
 readonly SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
@@ -13,10 +14,16 @@ if ! command -v jupyter &>/dev/null; then
     python3 -m pip install jupyter
 fi
 
-if ! python3 -m pip list 2>/dev/null | grep bokeh >/dev/null 2>&1; then
-    echo "bokeh not installed"
-    python3 -m pip install bokeh
-fi
+function check_pip_install {
+    package_name=$1
+    if ! python3 -m pip list 2>/dev/null | grep ${package_name} >/dev/null 2>&1; then
+        echo "${package_name} not installed"
+        python3 -m pip install ${package_name}
+    fi
+}
+
+check_pip_install bokeh
+check_pip_install jupyter-server-proxy
 
 # https://stackoverflow.com/a/72747002
 # May need to Downgrade markupsafe to 2.0.1
